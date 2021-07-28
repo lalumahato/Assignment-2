315a820b81686d48ae76e3d7afbcbfb6c202f03b
Lalu Mahato <lalu.mahato@weboniselab.com>
2021-07-28 11:42:49 +0530

    Added first feature

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello from Develop...");
+console.log("Add new feature here...");
+
+// add group feature
+console.group();
+console.log('Google');
+console.log('Facebook');
+console.log('Microsoft');
+console.groupEnd()
